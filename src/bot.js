0d1cd58acb2475f92ade054626beb9c503448bc1
baylrock <baylrock1994@gmail.com>
2021-09-05 23:44:15 -0400

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -91,7 +91,7 @@ function CCBot() {
         }
     };
 
-    var ignoreUpgrades = [182, 183, 184, 185, 209, 331, 333, 414, 361]
+    var ignoreUpgrades = [182, 183, 184, 185, 209, 331, 333, 414, 361, 563, 452]
     this.buyUpgrade = function () {
         var upgrade = Game.UpgradesInStore[0];
 
